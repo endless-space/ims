023a9967cb85017c7dd701f69fe22668879fad97
endlessspace <endlessspace@qq.com>
2018-07-21 19:22:09 +0800

    重新规划项目结构

--- ims-application-api/src/main/java/com/zero/ims/Application.java
+++ ims-application-api/src/main/java/com/zero/ims/Application.java
@@ -3,10 +3,13 @@ package com.zero.ims;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 通用入口
+ */
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 }
